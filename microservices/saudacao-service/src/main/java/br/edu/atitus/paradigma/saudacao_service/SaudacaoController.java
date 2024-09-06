7e654748b4946ef0d6f9162532d4f7d0b677a7a7
Raul Werner <qrananico25@gmail.com>
2024-09-06 20:47:54 -0300

    Aula 5

--- microservices/saudacao-service/src/main/java/br/edu/atitus/paradigma/saudacao_service/SaudacaoController.java
+++ microservices/saudacao-service/src/main/java/br/edu/atitus/paradigma/saudacao_service/SaudacaoController.java
@@ -1,5 +1,7 @@
 package br.edu.atitus.paradigma.saudacao_service;
 
+import br.edu.atitus.paradigma.saudacao_service.config.SaudacaoConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,15 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("saudacao-service")
 public class SaudacaoController {
-	
+
+	private final SaudacaoConfig saudacaoConfig;
+
+	public SaudacaoController(SaudacaoConfig saudacaoConfig) {
+		super();
+		this.saudacaoConfig = saudacaoConfig;
+	}
+
 	@GetMapping({"", "/", "/{nomePath}"})
 	public ResponseEntity<String> getSaudacao(
 			@RequestParam(required = false) String nome,
 			@PathVariable(required = false) String nomePath){
 		String template = "%s %s!";
 		if (nome == null) nome 
-		        = nomePath == null ? "World" : nomePath;
+		        = nomePath == null ? saudacaoConfig.getNomePadrao() : nomePath;
 		return ResponseEntity.ok(
-				String.format(template, "Hello", nome));
+				String.format(template, saudacaoConfig.getSaudacao(), nome));
 	}
 }
